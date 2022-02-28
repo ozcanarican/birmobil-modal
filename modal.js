5654ccfa99b457eee40b4ccca47f4a5536340753
ozcanarican <ozcan.arican@gmail.com>
2022-02-28 13:07:33 +0200

    add external events

--- modal.js
+++ modal.js
@@ -3,36 +3,39 @@ var timeInterval
 var countdown
 var timetext
 
-Array.from(document.getElementsByClassName("modal-button")).map((b) => {
-  b.addEventListener("click", () => {
-    let target = b.getAttribute("data-target");
-    if (target) {
-      let modal = document.getElementById(target);
-      if (modal) {
-        modal.style.display = "flex";
-        let ct = modal.querySelector(".modal-countdown")
-        if(ct) {
-            countdown = ct
-            countdown.setAttribute("data-status","0")
-            timetext = countdown.innerHTML
-            time = parseInt(countdown.getAttribute("data-time"))
-            countdown.innerHTML = timetext + " (" + time + " sn)"
-            countdown.classList.remove("modal-done")
-            timeInterval = setInterval(()=>{
-                time = time - 1
-                countdown.innerHTML = timetext + " (" + time + " sn)"
-                if(time == 0) {
-                    clearInterval(timeInterval)
-                    countdown.setAttribute("data-status","1")
-                    countdown.innerHTML = timetext
-                    countdown.classList.add("modal-done")
-                }
-            },1000)
+const addDefaultEvents => {
+  Array.from(document.getElementsByClassName("modal-button")).map((b) => {
+    b.addEventListener("click", () => {
+      let target = b.getAttribute("data-target");
+      if (target) {
+        let modal = document.getElementById(target);
+        if (modal) {
+          modal.style.display = "flex";
+          let ct = modal.querySelector(".modal-countdown")
+          if(ct) {
+              countdown = ct
+              countdown.setAttribute("data-status","0")
+              timetext = countdown.innerHTML
+              time = parseInt(countdown.getAttribute("data-time"))
+              countdown.innerHTML = timetext + " (" + time + " sn)"
+              countdown.classList.remove("modal-done")
+              timeInterval = setInterval(()=>{
+                  time = time - 1
+                  countdown.innerHTML = timetext + " (" + time + " sn)"
+                  if(time == 0) {
+                      clearInterval(timeInterval)
+                      countdown.setAttribute("data-status","1")
+                      countdown.innerHTML = timetext
+                      countdown.classList.add("modal-done")
+                  }
+              },1000)
+          }
         }
       }
-    }
+    });
   });
-});
+}
+addDefaultEvents()
 
 Array.from(document.getElementsByClassName("modal")).map((modal) => {
   let cancel = modal.querySelector(".modal-cancel");
